c7362c086ac5c6dcdbd8ce94a44a7b7b486ae8f4
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2022-12-16 22:01:20 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -10,32 +10,58 @@ using namespace std;
 class Solution{
 public:
     
-       int  solve(int i,int j,vector<vector<int>> &Matrix,vector<vector<int>> &dp)
-        {
+       //int  solve(int i,int j,vector<vector<int>> &Matrix,vector<vector<int>> &dp)
+        // {
             
-            if(j<0 || j>=Matrix[0].size())
-            return -1e9;
-            if(i==0)
-            return Matrix[0][j];
-            if(dp[i][j]!=-1)
-            return dp[i][j];
-                int up=Matrix[i][j]+solve(i-1,j,Matrix,dp);
-                int rd=Matrix[i][j]+solve(i-1,j+1,Matrix,dp);
-                int ld=Matrix[i][j]+solve(i-1,j-1,Matrix,dp);
-                dp[i][j]=max(up,max(rd,ld));
-                return dp[i][j];
+        //     if(j<0 || j>=Matrix[0].size())
+        //     return -1e9;
+        //     if(i==0)
+        //     return Matrix[0][j];
+        //     if(dp[i][j]!=-1)
+        //     return dp[i][j];
+        //         int up=Matrix[i][j]+solve(i-1,j,Matrix,dp);
+        //         int rd=Matrix[i][j]+solve(i-1,j+1,Matrix,dp);
+        //         int ld=Matrix[i][j]+solve(i-1,j-1,Matrix,dp);
+        //         dp[i][j]=max(up,max(rd,ld));
+        //         return dp[i][j];
             
-        }
+        // }
     
     int maximumPath(int N, vector<vector<int>> Matrix)
     {
-        int maxi=-1e8;
-        vector<vector<int>>dp(N,vector<int>(N,-1));
+        // int maxi=-1e8;
+        // vector<vector<int>>dp(N,vector<int>(N,-1));
+        // for(int j=0;j<N;j++)
+        // {
+        //     maxi=max(maxi,solve(N-1,j,Matrix,dp));
+        // }
+        // return maxi;
+        int t[N][N];
         for(int j=0;j<N;j++)
+        t[0][j]=Matrix[0][j];
+        
+        for(int i=1;i<N;i++)
         {
-            maxi=max(maxi,solve(N-1,j,Matrix,dp));
-        }
+          for(int j=0;j<N;j++)
+            {
+             int down=Matrix[i][j],ld=Matrix[i][j],rd=Matrix[i][j];
+            down+=t[i-1][j];
+            if(j>0)
+            ld+=t[i-1][j-1];
+            else
+            ld+=-1e9;
+            if(j+1<N)
+            rd+=t[i-1][j+1];
+            else
+            rd+=-1e9;
+            t[i][j]=max(down,max(ld,rd));
+            }
+          }
+        int maxi=-1e8;
+        for(int j=0;j<N;j++)
+        maxi=max(maxi,t[N-1][j]);
         return maxi;
+        
     }
 };
 
