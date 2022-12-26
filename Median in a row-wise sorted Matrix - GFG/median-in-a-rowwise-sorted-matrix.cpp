6d1dd0e44ca9b31c5b5d1f37f92a8b9a7ea4f617
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2022-12-26 20:06:00 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -8,50 +8,23 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
-// class Solution{   
-// public:
-//     int median(vector<vector<int>> &matrix, int R, int C){
-//         // code here          
-//     }
-// };
-
-        
 class Solution{   
 public:
-int check(vector<vector<int>>&matrix,int n){
-    int count=0;
-    for(int i=0;i<matrix.size();i++){
-        int j=0;;
-        while(j<matrix[0].size() && matrix[i][j]<=n){
-            count++;
-            j++;
+    int median(vector<vector<int>> &matrix, int R, int C){
+        
+    vector<int>v;
+    for(int i=0; i<R; i++)
+    {
+        for(int j=0; j<C; j++)
+        {
+            v.push_back(matrix[i][j]);
         }
     }
-    return count;
-}
-    int median(vector<vector<int>> &matrix, int R, int C){
-        // code here          
-        int s=INT_MAX;
-        int e=INT_MIN;
-        int n=((R*C)/2)+1;
-        for(int i=0;i<R;i++){
-            s=min(s,matrix[i][0]);
-            e=max(e,matrix[i][C-1]);
-        }
-        int ans=0;
-       while(s<=e){
-           int m=s+(e-s)/2;
-           int res=check(matrix,m);
-           
-           if(res<n){
-               s=m+1;
-           }
-          
-           else{
-               e=m-1;
-           }
-       }
-       return s;
+        
+    sort(v.begin(),v.end());
+    int n=v.size();
+    if(n%2!=0)
+        return v[n/2];
     }
 };
 
