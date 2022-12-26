74ee35254eb52ad4f7c9ac16d584e0eea2a91ee9
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2022-12-26 22:19:16 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -8,23 +8,28 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
+//User function template for C++
+
 class Solution{   
 public:
     int median(vector<vector<int>> &matrix, int R, int C){
-        
-    vector<int>v;
-    for(int i=0; i<R; i++)
-    {
-        for(int j=0; j<C; j++)
-        {
-            v.push_back(matrix[i][j]);
+        int medianPos = (R*C+1)/2 ; //or (R*C)/2 +1
+        int low = 1, high = 2000;
+        while(low<=high){ // obviously because it could be a single number only.
+            int mid = (low+high)/2;
+            //now finding the position of mid in hypothetically sorted array of matrix elements
+            int midPos = 0;
+            for(int i=0;i<R;i++){
+                vector<int> arr = matrix[i];
+                auto ptr = upper_bound(arr.begin(),arr.end(),mid);
+                int upIndex = ptr - arr.begin();
+                midPos += upIndex ;
+            }
+            // cout<< "for mid = "<<mid<<" ,it's position is= "<<midPos<<endl;
+            if(midPos<medianPos) low = mid+1;
+            else high = mid-1;
         }
-    }
-        
-    sort(v.begin(),v.end());
-    int n=v.size();
-    if(n%2!=0)
-        return v[n/2];
+        return low;
     }
 };
 
