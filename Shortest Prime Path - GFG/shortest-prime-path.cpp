531cee0fe7b18a3afaa0dfd9e0e5962bdc2f87ed
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-01-06 21:50:02 +0530

    Added solution - LeetHub

--- Shortest Prime Path - GFG/shortest-prime-path.cpp
+++ Shortest Prime Path - GFG/shortest-prime-path.cpp
@@ -10,92 +10,49 @@ using namespace std;
    vector<int>isprime(10000,1); class Solution{   
 public:  
   
-//     void sieve()
-//     {
-//         for(int i=2;i<10000;i++)
-//         {
-//             for(int j=i+i;j<10000;j=j+i)
-//             {
-//                 isprime[j]=0;
-//             }
-//         }
-//     }
-    
-   
-//     int shortestPath(int n1,int n2)
-//     {   
-        
-//         if(!a)
-//         {
-//             sieve();
-//             a=true;
-//         }
-        
-//         queue<pair<int,int>>q;
-//         q.push({n1,0});
-//         vector<int>vis(10000,0);
-//         vis[n1]=1;
-//         while(!q.empty());
-//         {
-//             auto t=q.front();
-//             q.pop();
-//             if(t.first==n2)
-//             return t.second;
-//             string num=to_string(t.first);
-//             for(int j=0;j<4;j++)
-//             {
-//                 for(int k=0;k<=9;k++)
-//                 {
-//                     char c='0'+k;
-//                     string num2=num;
-//                     //puraani string ke vaaste
-//                     num2[j]=c;
-//                     int temp=stoi(num2);
-//                     if(isprime[temp] && num2[0]!='0' && vis[temp]==0)
-//                     {
-//                         q.push({temp,t.second+1});
-//                         vis[temp]=1;
-//                     }
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-//};
-//bool a=false;
-//vector<int> isprime(10000,1);
-    
-    
-    void seive(){
-        for(int i=2;i<10000;i++){
-            for(int j=i+i;j<10000;j=j+i){
+    void seive()
+    {
+        for(int i=2;i<10000;i++)
+        {
+            for(int j=i+i;j<10000;j=j+i)
+            {
                 isprime[j]=0;
             }
         }
     }
-    int shortestPath(int Num1,int Num2)
+    
+   
+    int shortestPath(int n1,int n2)
     {   
-        // Complete this function using prime vector
-        if(!a){
+        
+        if(!a)
+        {
             seive();
             a=true;
         }
-        queue<pair<int,int>> q;
-        q.push({Num1,0});
-        vector<int> vis(10000,0);
-        vis[Num1]=1;
-        while(q.size()){
-            auto t = q.front();
+        
+        queue<pair<int,int>>q;
+        q.push({n1,0});
+        vector<int>vis(10000,0);
+        vis[n1]=1;
+        while(!q.empty())
+        {
+            auto t=q.front();
             q.pop();
-            if(t.first==Num2)return t.second;
-            string num = to_string(t.first);
-            for(int j=0;j<4;j++){
-                for(int k=0;k<=9;k++){
-                    char c= '0'+k;
+            if(t.first==n2)
+            return t.second;
+            string num=to_string(t.first);
+            for(int j=0;j<4;j++)
+            {
+                for(int k=0;k<=9;k++)
+                {
+                    char c='0'+k;
                     string num2=num;
+                    //puraani string ke vaaste
                     num2[j]=c;
-                    int temp = stoi(num2);
-                    if(isprime[temp] and num2[0]!='0' and vis[temp]==0){
+                    int temp=stoi(num2);
+                    if(isprime[temp] && num2[0]!='0' && vis[temp]==0)
+                    {
                         q.push({temp,t.second+1});
                         vis[temp]=1;
                     }
@@ -104,7 +61,8 @@ public:
         }
         return -1;
     }
-   };
+};
+
 
 
 //{ Driver Code Starts.
