cdf0ee6ec89447e32e8bab7b45e0749068349808
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-01-07 23:48:47 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.cpp
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -46,84 +46,42 @@ struct Node{
 
 class Solution {
 public:
-//         Node* mergetwolists(Node*a ,Node*b)
-//         {
-//             Node* temp=new Node(0);
-//             Node* res=temp;
-//             while(a!=NULL && b!=NULL)
-//             {
-//                 if(b->data>a->data)
-//                 {
-//                     temp->bottom=a;
-//                     temp=temp->bottom;
-//                     a=a->bottom;
-//                 }
-//                 else
-//                 {
-//                     temp->bottom=b;
-//                     temp=temp->bottom;
-//                     b=b->bottom;
-//                 }
+        Node* mergetwolists(Node*a ,Node*b)
+        {
+            Node* temp=new Node(0);
+            Node* res=temp;
+            while(a!=NULL && b!=NULL)
+            {
+                if(b->data>a->data)
+                {
+                    temp->bottom=a;
+                    temp=temp->bottom;
+                    a=a->bottom;
+                }
+                else
+                {
+                    temp->bottom=b;
+                    temp=temp->bottom;
+                    b=b->bottom;
+                }
                 
-//             }
-//             if(a)
-//             return temp->bottom=a;
-//             if (b)
-//             return temp->bottom=b;
-//             return res->bottom;
-//         }
+            }
+            if(a)
+            temp->bottom=a;
+            if (b)
+             temp->bottom=b;
+            return res->bottom;
+        }
 
-//     Node *flatten(Node *root)
-//     {
-//         if(root==NULL || root->next==NULL)
-//         return root;
+    Node *flatten(Node *root)
+    {
+        if(root==NULL || root->next==NULL)
+        return root;
         
-//         root->next=flatten(root->next);
-//         root=mergetwolists(root,root->next);
-//         return root;
-//     }
-// };
-
-Node* mergeTwoLists(Node* a, Node* b) {
-    
-    Node *temp = new Node(0);
-    Node *res = temp; 
-    
-    while(a != NULL && b != NULL) {
-        if(a->data < b->data) {
-            temp->bottom = a; 
-            temp = temp->bottom; 
-            a = a->bottom; 
-        }
-        else {
-            temp->bottom = b;
-            temp = temp->bottom; 
-            b = b->bottom; 
-        }
+        root->next=flatten(root->next);
+        root=mergetwolists(root,root->next);
+        return root;
     }
-    
-    if(a) temp->bottom = a; 
-    else temp->bottom = b; 
-    
-    return res -> bottom; 
-    
-}
-Node *flatten(Node *root)
-{
-   
-        if (root == NULL || root->next == NULL) 
-            return root; 
-  
-        // recur for list on right 
-        root->next = flatten(root->next); 
-  
-        // now merge 
-        root = mergeTwoLists(root, root->next); 
-  
-        // return the root 
-        // it will be in turn merged with its left 
-        return root; 
-}
 };
 
 
