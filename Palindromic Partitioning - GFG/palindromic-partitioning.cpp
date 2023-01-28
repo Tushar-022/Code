3ab1815c470d689e71bf5e8ab6b08e9a197f196b
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-01-28 11:50:09 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -22,42 +22,26 @@ bool ispal(int i,int j,string&s)
         return true;
     }
     
- int solve(int i,int j,string &s,int n,vector<vector<int>>&dp)
+ int solve(int i,int n,string &s,vector<int>&dp)
 {
-if(i>=j)
+if(i>=n)
 return 0;
-if(ispal(i,j,s))
-return 0;
-int partitions=INT_MAX;
-if(dp[i][j]!=-1)
-return dp[i][j];
-for(int k=i;k<j;k++)
+if(ispal(i,n,s))
+ return 0;
+int partitions=INT_MAX,cost=0;
+if(dp[i]!=-1)
+return dp[i];
+for(int j=i;j<n;j++)
 {
-    int left=0,right=0;
-    if(ispal(i,k,s))
-{    
+    if(ispal(i,j,s))
+    {
+        cost=1+solve(j+1,n,s,dp);
+    
+        partitions=min(partitions,cost);
+    }
+}
+return dp[i]=partitions;
 
-if(dp[i][k]!=-1)
-left= dp[i][k];
-else
-{
-    left=solve(i,k,s,n,dp);
-    dp[i][k]=left;
-}
-if(dp[k+1][j]!=-1)
-right=dp[k+1][j];
-else
-{
-right=solve(k+1,j,s,n,dp);
-dp[k+1][j]=right;
-}
-int par=left+right+1;  
-partitions=min(par,partitions);
-}
-else
-continue;
-}
-return dp[i][j]=partitions;
 }
     int palindromicPartition(string s)
     {
@@ -65,8 +49,8 @@ return dp[i][j]=partitions;
         int n=s.length();
         if(n==1)
         return 0;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(0,n-1,s,n,dp);
+       vector<int>dp(n+1,-1);
+        return solve(0,n-1,s,dp);
     }
 };
 
