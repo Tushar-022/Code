159a38e8187ab07a709c27669501821b34d59a10
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-03-02 20:58:59 +0530

    Added solution - LeetHub

--- Prerequisite Tasks - GFG/prerequisite-tasks.cpp
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -5,15 +5,15 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-	bool isPossible(int n, vector<pair<int, int> >& prerequisites) {
+	bool isPossible(int n, vector<pair<int, int> >& pre) {
 	    
 	    vector<int>adj[n];
-	    for(auto it:prerequisites)
-	    adj[it.first].push_back(it.second);
-	    
+	    for(auto it:pre)
+	    {
+	        adj[it.first].push_back(it.second);
+	    }
 	    vector<int>topo;
 	    vector<int>vis(n,0);
-	    queue<int>q;
 	    vector<int>indegree(n,0);
 	    for(int i=0;i<n;i++)
 	    {
@@ -22,31 +22,32 @@ public:
 	            indegree[it]++;
 	        }
 	    }
+	    queue<int>q;
 	    for(int i=0;i<n;i++)
 	    {
 	        if(indegree[i]==0)
-	        q.push(i);
-	        
+	        {
+	            q.push(i);
+	        }
 	    }
-	    
 	    while(!q.empty())
 	    {
-	        
 	        int node=q.front();
-	        topo.push_back(node);
 	        q.pop();
+	        topo.push_back(node);
 	        for(auto it:adj[node])
 	        {
-	                indegree[it]--;
-	                if(indegree[it]==0)
-	                q.push(it);
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	            q.push(it);
 	        }
+	        
+	        
 	    }
 	    if(topo.size()==n)
 	    return true;
 	    else
 	    return false;
-	    
 	}
 };
 
