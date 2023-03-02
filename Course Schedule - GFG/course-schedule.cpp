ae0310566ca026a1f8ea32d9e57f0c8252962195
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-03-02 21:29:43 +0530

    Added solution - LeetHub

--- Course Schedule - GFG/course-schedule.cpp
+++ Course Schedule - GFG/course-schedule.cpp
@@ -8,15 +8,13 @@ using namespace std;
 class Solution
 {
   public:
-    vector<int> findOrder(int n, int m, vector<vector<int>> prerequisites) 
+    vector<int> findOrder(int n, int m, vector<vector<int>> pre) 
     {
         
         vector<int>adj[n];
-        for(int i=0;i<prerequisites.size();i++)
-	    adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        
+        for(auto it:pre)
+	    adj[it[1]].push_back(it[0]);
 	    vector<int>topo;
-	    //vector<int>vis(n,0);
 	    queue<int>q;
 	    vector<int>indegree(n,0);
 	    for(int i=0;i<n;i++)
