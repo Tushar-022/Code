44d16510c249b81866f4526ed466fb0240bd3788
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-03-03 17:01:15 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -35,8 +35,8 @@ class Solution{
     Node* getmid(Node*head)
     {
         Node* slow=head;
-        Node* fast=head;
-        while( fast->next!=NULL && fast->next->next!=NULL )
+        Node* fast=head->next;
+        while( fast!=NULL && fast->next!=NULL)
         {
             slow=slow->next;
             fast=fast->next->next;
