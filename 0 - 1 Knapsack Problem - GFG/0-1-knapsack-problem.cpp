907a4f368baf22a214db906e71f3c5446cc69abb
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-03-05 19:59:26 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -4,79 +4,37 @@ using namespace std;
 
 
 // } Driver Code Ends
-
-
-class Solution{
-    
-public:
-
-    // int solve(int W,int wt[],int val[],int n,vector<vector<int>>&dp)
-    // {
-    //     if(W==0)
-    //     return 0;
-    //     if(n==0)
-    //     {
-    //         if(W>=wt[0])
-    //         return val[0];
-    //         return 0;
-    //     }
-    //     if(dp[n][W]!=-1)
-    //     return dp[n][W];
-    //     if(W>=wt[n])
-    //     {
-    //          return dp[n][W]= max(val[n]+solve(W-wt[n],wt,val,n-1,dp),solve(W,wt,val,n-1,dp));
-    //     }
-    //     if(wt[n]>W)
-    //     {
-    //         return dp[n][W]= solve(W,wt,val,n-1,dp);
-    //     }
-        
-        
-    // }
-    
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-    //   if(n==0)
-    //   return 0;
-    //   vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
-    //   return solve(W,wt,val,n-1,dp);
-        vector<vector<int>>t(n+1,vector<int>(W+1,-1));
-        for(int i=0;i<n+1;i++)
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int solve(int w,int* wt,int* val,int n,vector<vector<int>>&dp)
+    {
+        if(w==0)
+        return 0;
+        if(n==0)
         {
-            for(int j=0;j<W+1;j++)
-            {
-                if(i==0 || j==0)
-                t[i][j]=0;
-            }
+            if(w>=wt[0])
+            return val[0];
+            return 0;
         }
-        for(int i=1;i<n+1;i++)
+        if(dp[n][w]!=-1)
+        return dp[n][w];
+        if(w>=wt[n])
         {
-            for(int j=1;j<W+1;j++)
-        {
-                 if(j>=wt[i-1])
-            {
-              t[i][j]= max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
-            }
-            else
-            {
-             t[i][j]= t[i-1][j];
-            }
+            return dp[n][w]=max(val[n]+solve(w-wt[n],wt,val,n-1,dp),solve(w,wt,val,n-1,dp));
+            
         }
-        }
-        //for(int i=0;i<n+1;i++)
-        //{
-          //  for(int j=0;j<W+1;j++)
-            //{
-            //    cout<<t[i][j]<<" ";
-            //}
-            //cout<<endl;
-        //}
-        
-        //cout<<t[n][W]<<endl;
-        return t[n][W];
+        else 
+        return dp[n][w]=solve(w,wt,val,n-1,dp);
         
     }
-    };
+    int knapSack(int w, int wt[], int val[], int n) 
+    { 
+        vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+       return solve(w,wt,val,n-1,dp);
+    }
+};
 
 //{ Driver Code Starts.
 
