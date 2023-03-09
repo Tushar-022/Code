ecf1b97679763a85df403ea041cd037a06553d59
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-03-10 00:43:00 +0530

    Added solution - LeetHub

--- Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -38,22 +38,27 @@ class Solution
 {
     public:
     //Function to return the lowest common ancestor in a Binary Tree.
+    Node* solve(Node*root,int n1,int n2)
+    {
+        if(root==NULL)
+        return NULL;
+        if(root->data==n1||root->data==n2)
+        return root;
+        Node* leftans=solve(root->left,n1,n2);
+        Node* rightans=solve(root->right,n1,n2);
+        if(leftans!=NULL && rightans!=NULL)
+        return root;
+        if(leftans!=NULL && rightans==NULL)
+        return leftans;
+        if(rightans!=NULL && leftans==NULL)
+        return rightans;
+        return NULL;
+        
+    }
     Node* lca(Node* root ,int n1 ,int n2 )
     {
-       if(root==NULL)
-       return NULL;
-       if(root->data==n1 || root->data==n2)
-       return root;
-       Node* leftans=lca(root->left,n1,n2);
-       Node* rightans=lca(root->right,n1,n2);
-       if(leftans!=NULL && rightans!=NULL)
-       return root;
-       else if(leftans!=NULL && rightans==NULL)
-       return leftans;
-       else if(leftans==NULL && rightans!=NULL)
-       return rightans;
-       else 
-       return NULL;
+       
+       return solve(root,n1,n2);
     }
 };
 
