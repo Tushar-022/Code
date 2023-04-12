aae00c089dd98770390fc6cce683f2c46aa2d04b
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-04-12 19:54:37 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Program to find the maximum profit job sequence from a given array 
+// of jobs with deadlines and profits 
+#include<bits/stdc++.h>
+using namespace std; 
+
+// A structure to represent a job 
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+}; 
+
+
+// } Driver Code Ends
+/*
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+};
+*/
+
+class Solution 
+{
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    
+    static bool comparison(Job a,Job b)
+    {
+        return (a.profit>b.profit);
+    }
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        //vector<int>ans;
+        
+        sort(arr,arr+n,comparison);
+        
+        int maxi=arr[0].dead;
+        for(int i=1;i<n;i++)
+        maxi=max(maxi,arr[i].dead);
+        int cntjobs=0;
+        vector<int>slot(maxi+1,-1);
+        int profit=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=arr[i].dead;j>0;j--)
+            {
+                if(slot[j]==-1)
+                {
+                    slot[j]=i;
+                    profit+=arr[i].profit;
+                    cntjobs++;
+                    break;
+                }
+            }
+        }
+        vector<int>ans(2,0);
+        ans[0]=cntjobs;
+        ans[1]=profit;
+        return ans;
+        // ans[0]=maxi;
+        // ans[1]=profit;
+        // return ans;
+        
+    } 
+};
+
+//{ Driver Code Starts.
+// Driver program to test methods 
+int main() 
+{ 
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        Job arr[n];
+        
+        //adding id, deadline, profit
+        for(int i = 0;i<n;i++){
+                int x, y, z;
+                cin >> x >> y >> z;
+                arr[i].id = x;
+                arr[i].dead = y;
+                arr[i].profit = z;
+        }
+        Solution ob;
+        //function call
+        vector<int> ans = ob.JobScheduling(arr, n);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+	return 0; 
+}
+
+
+
+// } Driver Code Ends
