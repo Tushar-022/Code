3076eb6c8c53e19023395b1d26b23ef9d388f65b
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-05-25 17:22:02 +0530

    Added solution - LeetHub

--- Aggressive Cows - GFG/aggressive-cows.cpp
+++ Aggressive Cows - GFG/aggressive-cows.cpp
@@ -7,45 +7,57 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// User function Template for C++
+
+// class Solution {
+// public:
+
+//     int solve(int n, int k, vector<int> &stalls) {
+    
+        
+//     }
+// User function Template for C++
+
 class Solution {
 public:
-    
-    bool isvalid(int mid,int k,vector<int>&stalls,int n)
-    {
-        int dis=stalls[0], cow=1;
-        for(int i=1;i<n;i++)
-        {
-            
-            if(stalls[i]-dis>mid)
-            {
-                dis=stalls[i];
-                cow++;
-                if(cow==k)
-                return true;
-            }
-        }
-        return false;
-    }
+       bool isPossible(int n,int k,vector<int>&stalls,int dist)
+       {
+           int coord=stalls[0];
+           int count=1;
+           for(int i=1;i<n;i++)
+           {
+               if(stalls[i]-coord>=dist)
+               {
+                   count++;
+                   coord=stalls[i];
+               }
+               if(count==k) return true;
+           }
+           return false;
+       }
+
+
     int solve(int n, int k, vector<int> &stalls) {
     
-    sort(stalls.begin(),stalls.end());
-    int low=1;
-    int high=stalls[n-1]-stalls[0];
-    while(high>=low)
-    {
-        int mid=low+(high-low)/2;
-        if(isvalid(mid,k,stalls,n))
+        sort(stalls.begin(),stalls.end());
+        int low=1;
+        int high=stalls[n-1]-stalls[0];
+        while(low<=high)
         {
-            low=mid+1;
+            int mid=(low+high)/2;
+            if(isPossible(n,k,stalls,mid))
+            
+                low=mid+1;
+                else 
+                high=mid-1;
+            
         }
-        else
-        high=mid-1;
         
-    }
-    return low;
-    return 0;
+        return high;
     }
 };
+            
+      
 
 //{ Driver Code Starts.
 
