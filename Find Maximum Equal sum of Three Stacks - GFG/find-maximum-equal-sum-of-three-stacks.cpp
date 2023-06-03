e8990332b1fadfe7f1254dfdc26f918efe6d67d5
Tushar-022 <96460114+Tushar-022@users.noreply.github.com>
2023-06-03 08:37:53 +0530

    Added solution - LeetHub

---
+++ Find Maximum Equal sum of Three Stacks - GFG/find-maximum-equal-sum-of-three-stacks.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+//User function Template for C++
+
+class Solution{
+public:
+    int maxEqualSum(int N1,int N2,int N3,vector<int> &S1,vector<int> &S2,vector<int> &S3){
+        int min=0;
+        int sum1=0, sum2=0, sum3=0;
+        for(int i=0; i<N1; i++){
+            sum1+=S1[i];
+        }
+        for(int i=0; i<N2; i++){
+            sum2+=S2[i];
+        }
+        for(int i=0; i<N3; i++){
+            sum3+=S3[i];
+        }
+        if(sum1==sum2==sum3){
+            return sum1;
+        }
+      int a=0, b=0, c=0;
+      while(a<N1 && b<N2 && c<N3){
+          if(sum1==sum2 && sum2==sum3){
+              return sum1;
+          }
+          
+          
+            if(sum1 > sum2 or sum1 > sum3)
+            {
+                sum1 -= S1[a];
+                a++;
+            }
+            else if(sum2>sum1 or sum2>sum3)
+            {
+                sum2 -= S2[b];
+                b++;
+            }
+            else if(sum3>sum1 or sum3>sum2)
+            {
+                sum3 -= S3[c];
+                c++;
+            }
+      }
+        
+        return 0;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n1,n2,n3;
+        cin>>n1>>n2>>n3;
+        vector<int> s1(n1),s2(n2),s3(n3);
+        for(int i=0;i<n1;i++){
+            cin>>s1[i];
+        }
+        for(int i=0;i<n2;i++){
+            cin>>s2[i];
+        }
+        for(int i=0;i<n3;i++){
+            cin>>s3[i];
+        }
+        Solution ob;
+        int ans=ob.maxEqualSum(n1,n2,n3,s1,s2,s3);
+        cout<<ans<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
