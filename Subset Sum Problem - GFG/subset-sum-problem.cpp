1b046f9e1f97a33993fabc02da84b692b5572c7d
Tushar Khandelwal <96460114+Tushar-022@users.noreply.github.com>
2023-06-09 15:50:34 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -10,26 +10,51 @@ using namespace std;
 class Solution{   
 public:
 
-     bool solve(vector<vector<int>>&dp,vector<int>&arr,int sum,int ind)
-     {
-         if(sum==0)
-         return true;
-         if(ind==0)
-         return (sum==arr[ind]);
-         if(dp[ind][sum]!=-1)
-         return dp[ind][sum];
-         bool not_take=solve(dp,arr,sum,ind-1);
-         bool take=false;
-         if(sum>=arr[ind])
-         take=solve(dp,arr,sum-arr[ind],ind-1);
-         return dp[ind][sum]=take|not_take;
+    // int solve(vector<int>&arr, int tgt,int ind)
+    // {
+    //     if(tgt==0)
+    //     return true;
+    //     if(ind==0)
+    //     {
+    //         if(tgt!=arr[0])
+    //         return false;
+    //         return true;
+    //     }
+        
+    //     int ntake=solve(arr,tgt,ind-1);
+    //     int take=false;
+    //     if(tgt>=arr[ind])
+    //      take=solve(arr,tgt-arr[ind],ind-1);
          
-         
-     }
-    bool isSubsetSum(vector<int>arr, int sum){
+    //      return take|ntake;
+    // }
+    bool isSubsetSum(vector<int>arr, int tgt){
+        
+        // return solve(arr,tgt,arr.size()-1); 
         int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return solve(dp,arr,sum,n-1);
+        vector<vector<int>>dp(n+1,vector<int>(tgt+1,0));
+        
+        for(int i=0;i<n;i++)
+        {
+           dp[i][0]=1;
+        }
+        if(tgt>=arr[0])
+        dp[0][arr[0]]=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=tgt;j++)
+            {
+                int ntake=dp[i-1][j];
+                int take=false;
+                if(j>=arr[i])
+                {
+                    take=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=take|ntake;
+            }
+        }
+        return dp[n-1][tgt];
     }
 };
 
