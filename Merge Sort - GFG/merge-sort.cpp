440eaf5d6893272ab04ee6d37049bf2af69d9e9f
Tushar Khandelwal <96460114+Tushar-022@users.noreply.github.com>
2023-07-06 08:04:12 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -16,128 +16,61 @@ void printArray(int arr[], int size)
 
 
 // } Driver Code Ends
-
-//class Solution
-// {
-//     public:
-//     void merge(int* arr, int l, int m, int r)
-//     {
-//         int n1=m-l+1;
-//         int n2=r-m;
-//         int left[n1];
-//         int right[n2];
-//         for(int i=0;i<n1;i++)
-//         {
-//             left[i]=arr[l+i];
-//         }
-//         for(int i=0;i<n2;i++)
-//         {
-//             right[i]=arr[m+i+1];
-            
-//         }
-//         int i=0,j=0,k=l;
-//         while(i<n1 && j<n2)
-//         {
-//             if(right[j]>=left[i])
-//             {
-//                 arr[k++]=left[i];
-//                 i++;
-//                 //k++;
-//             }
-//             else
-//             {
-//                 arr[k++]=right[j];
-//                 j++;
-//               // k++;
-//             }
-//         }
-//         while(i<n1 )
-//         {
-//             arr[k++]=left[i];
-//             i++;
-//         }
-//         while(j<n1 )
-//         {
-//             arr[k++]=right[j];
-//             j++;
-//         }
-        
-//     }
-//     public:
-//     void mergeSort(int arr[], int l, int r)
-//     {
-//         //cout<<arr[l]<<" "<<arr[r]<<endl;
-//         //cout<<arr[r]<<endl;
-        
-//         if(r>l)
-//           {
-//               int mid=l+(r-l)/2;
-//           mergeSort(arr,l,mid);
-//           mergeSort(arr,mid+1,r);
-//           merge(arr,l,mid,r);
-//           }
-//     }
-    
-// };
-
 class Solution
 {
     public:
     void merge(int arr[], int l, int m, int r)
     {
-          
-        int n1=m-l+1,n2=r-m;
-         int left[n1],right[n2];
-         for(int i=0;i<n1;i++)
-         {
-            left[i]=arr[i+l];
+        int n1=m-l+1;
+        int n2=r-m;
+        vector<int>left(n1);
+        vector<int>right(n2);
+        
+        for(int i=0;i<n1;i++)
+        {
+            left[i]=arr[l+i];
+        }
+        
+        for(int i=0;i<n2;i++)
+        {
+            right[i]=arr[m+1+i];
+        }
+        
+        int i=0,j=0,k=l;
+        
+        while(i<n1 && j<n2)
+        {
+            if(right[j]>=left[i])
+            arr[k++]=left[i++];
             
-         }
+            else
+            {
+                arr[k++]=right[j++];
+            }
+        }
+        
+        while(i<n1)
+        {
+            arr[k++]=left[i++];
+        }
          
-         for(int i=0;i<n2;i++)
-         {
-             right[i]=arr[m+i+1];
-         }
-         int i=0,j=0,k=l;
-         while(i<n1&&j<n2)
-         {
-             if(left[i]<=right[j])
-             {
-                 arr[k++]=left[i];
-                 i++;
-             }
-             
-             else
-             {
-                 arr[k++]=right[j];
-                 j++;
-             }
-         }
-         
-         
-         while(i<n1)
-         {
-             arr[k++]=left[i];
-             i++;
-         }
          
          while(j<n2)
          {
-             arr[k++]=right[j];
-             j++;
+             arr[k++]=right[j++];
          }
+         
+         
     }
-    public:
+
     void mergeSort(int arr[], int l, int r)
     {
-        //code here
-        
-        if(l<r)
+       if (r > l)
         {
-            int m=l+((r-l)/2);
-            mergeSort(arr,l,m);
-            mergeSort(arr,m+1,r);
-            merge(arr,l,m,r);
+            int mid = l + (r - l) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
         }
     }
 };
